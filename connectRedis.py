de91a01cad17343a7fc2a5feb3e9979f8692ff5b
Tianna Cano <39601543+tianna-c@users.noreply.github.com>
2022-05-06 21:29:15 -0700

    Add files via upload

--- connectRedis.py
+++ connectRedis.py
@@ -3,27 +3,26 @@ import redis
 import sqlite3
 
 redisClient = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-r = redis.Redis()
 conn = sqlite3.connect('shard_1.db')
 cur = conn.cursor()
 cur.execute("SELECT * FROM games")
 allGames = cur.fetchall()
+stats = {}
+games = "Games"
 
-try:
-	for row in allGames:
-		#r.zadd("games", row) #returns (nil)
-		#r.zadd("games", {"user_id": row[0]}, {"game_id": row[1]}, {"finished": row[2]}, {"guesses": row[3]}, {"won": row[4]})
-		print("User ID: ", row[0])
-		print("Game ID: ", row[1])
-		print("Finished: ", row[2])
-		print("Guesses: ", row[3])
-		print("Won: ", row[4])
-		print("\n")
-		#r.zadd("games", row[0], row[1], row[2], row[3], row[4])
-		#r.getall("games")
-except:
-	print("Done!")
+for row in allGames:
+	#r.zadd("games", {"user_id": row[0]}, {"game_id": row[1]}, {"finished": row[2]}, {"guesses": row[3]}, {"won": row[4]})
+	user_id = row[0]
+	game_id = row[1]
+	finished = row[2]
+	guesses = row[3]
+	won = row[4]
+	redisClient.zadd(games, user_id, "user_id", game_id, "game_id")
+	redisClient.zadd(games, finished, "finished", guesses, "guesses", won, "won")
+	#r.zadd("games", row[0], row[1], row[2], row[3], row[4])
+	#r.getall("games")
+print("Success!")
+	
 
 conn = sqlite3.connect('shard_2.db')
 cur = conn.cursor()
@@ -49,7 +48,7 @@ try:
 except:
 	print("Done!")
 
-conn = sqlite3.connect('userShard.db')
+conn = sqlite3.connect('users.db')
 cur = conn.cursor()
 cur.execute("SELECT * FROM users")
 
